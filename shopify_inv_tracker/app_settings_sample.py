a0b80bc3fe44e359fbc349a4d574a7ef9b0046f8
Sudip B.K <sudip.k3@gmail.com>
2019-09-23 11:50:44 +0545

    Shopify django app

---
+++ shopify_inv_tracker/app_settings_sample.py
@@ -0,0 +1,10 @@
+# Shopify app settings
+
+SHOPIFY_API_KEY = 'api key'
+SHOPIFY_API_SECRET = 'api secret key'
+SHOPIFY_API_SCOPE = ['scope1', 'scope2']
+DB_NAME = 'db name'
+DB_USER = 'db user'
+DB_PASSWORD = 'db password'
+URL = 'app url'
+SECRET_KEY = 'django secret key'
